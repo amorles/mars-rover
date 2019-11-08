9ba31237ad0f3e5b50955d3c0bbaf5d31ff63623
amorles <54360375+amorles@users.noreply.github.com>
2019-11-08 13:39:10 -0500

    made sure push worked

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,86 @@
 // Rover Object Goes Here
 // ======================
-
+let rover = {
+  direction: "N",
+  x: 0,
+  y: 0,
+}
 // ======================
+
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  if(rover.direction == "N"){
+    rover.direction = "W"
+  }
+  else if(rover.direction == "W"){
+    rover.direction = "S"
+  }
+  else if(rover.direction == "S"){
+    rover.direction = "E"
+  }
+  else if(rover.direction == "E"){
+    rover.direction = "N"
+  }
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  if(rover.direction == "N"){
+    rover.direction = "E"
+  }
+  else if(rover.direction == "E"){
+    rover.direction = "S"
+  }
+  else if(rover.direction == "S"){
+    rover.direction = "W"
+  }
+  else if(rover.direction == "W"){
+    rover.direction = "N"
+  }
 }
 
 function moveForward(rover){
   console.log("moveForward was called")
+  if(rover.direction == "N"){
+    rover.y++
+    console.log("The rover is now at: (" + rover.x + "," + rover.y + ")")
+  }
+  else if(rover.direction == "S"){
+    rover.y--
+    console.log("The rover is now at: (" + rover.x + "," + rover.y + ")")
+  }
+  if(rover.direction == "W"){
+    rover.x--
+    console.log("The rover is now at: (" + rover.x + "," + rover.y + ")")
+  }
+  else if(rover.direction == "E"){
+    rover.x++
+    console.log("The rover is now at: (" + rover.x + "," + rover.y + ")")
+  }
+}
+
+function moveBackward(rover){
+  console.log("moveBackward was called")
+  if(rover.direction == "N"){
+    rover.y--
+    console.log("The rover is now at: (" + rover.x + "," + rover.y + ")")
+  }
+  else if(rover.direction == "S"){
+    rover.y++
+    console.log("The rover is now at: (" + rover.x + "," + rover.y + ")")
+  }
+  if(rover.direction == "W"){
+    rover.x++
+    console.log("The rover is now at: (" + rover.x + "," + rover.y + ")")
+  }
+  else if(rover.direction == "E"){
+    rover.x--
+    console.log("The rover is now at: (" + rover.x + "," + rover.y + ")")
+  }
+}
+
+function commands(string){
+  for(i=0, i < string.length, i++ ){
+
+  }
 }
